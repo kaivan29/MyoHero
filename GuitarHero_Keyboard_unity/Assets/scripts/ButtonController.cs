2316cdd0f46208dac9e66b3d6574cdda022df540
Kaivan Shah <kaivan29@tamu.edu>
2015-09-27 08:19:56 -0500

    intial working stage

---
+++ GuitarHero_Keyboard_unity/Assets/scripts/ButtonController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonController : MonoBehaviour {
+	
+	private Rigidbody rb;
+
+	void Start()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.CompareTag ("KeyNote"))
+			other.gameObject.SetActive (false);
+	}
+}
---
+++ GuitarHero_Keyboard_unity/Assets/scripts/Translation.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class Translation : MonoBehaviour {
+	
+	int i = 0;
+	// Update is called once per frame
+	void Update () {
+		while(i<100)
+		{
+				transform.Translate (new Vector3 (0,0,2)*Time.deltaTime);
+		//public void Rotate(Vector3 eulerAngles, Space relativeTo = Space.Self); 
+		//15, 45 and 30 are euler angles
+		//we are going to keep space default here.
+         		i = i+1;
+		}
+	}
+	//A prefab is an asset that contains a template
+
+}
